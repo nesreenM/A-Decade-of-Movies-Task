e55cb46703c15f31a6959ddad08f654e7c5b1d77
Nesreen Mamdouh <>
2019-05-19 01:37:40 +0200

    Added Movie model

---
+++ A Decade of Movies/Movie+CoreDataClass.swift
@@ -0,0 +1,46 @@
+//
+//  Movie+CoreDataClass.swift
+//  A Decade of Movies
+//
+//  Created by Nesreen Mamdouh on 5/19/19.
+//  Copyright © 2019 swvl. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject, Decodable {
+  
+    enum CodingKeys: String, CodingKey {
+        case title, year, rating
+    }
+    
+    required convenience public init(from decoder: Decoder) throws {
+        // Create NSEntityDescription with NSManagedObjectContext
+        guard let managedObjectContext = CoreDataStack.sharedInstance.persistentContainer.viewContext as? NSManagedObjectContext,
+            let entity = NSEntityDescription.entity(forEntityName: "Movie", in: managedObjectContext) else {
+                fatalError("Failed to decode Movie!")
+        }
+        self.init(entity: entity, insertInto: nil)
+        // Decode
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        title = try values.decode(String.self, forKey: .title)
+        year = try values.decode(Int16.self, forKey: .year)
+        rating = try values.decode(Double.self, forKey: .rating)
+    }
+}
+
+extension Movie: Encodable {
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(title, forKey: .title)
+        try container.encode(year, forKey: .year)
+        try container.encode(rating, forKey: .rating)
+    }
+}
+
+extension CodingUserInfoKey {
+    static let context = CodingUserInfoKey(rawValue: "context")
+}
---
+++ A Decade of Movies/Movie+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  Movie+CoreDataProperties.swift
+//  A Decade of Movies
+//
+//  Created by Nesreen Mamdouh on 5/19/19.
+//  Copyright © 2019 swvl. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Movie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Movie> {
+        return NSFetchRequest<Movie>(entityName: "Movie")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var year: Int16
+    @NSManaged public var rating: Double
+
+}
