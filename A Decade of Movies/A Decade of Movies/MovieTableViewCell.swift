62f63a4eacfa22bd0fe51a4ddadec57f4f7c8389
Nesreen Mamdouh <>
2019-05-19 01:26:16 +0200

    Add movie cell class

---
+++ A Decade of Movies/A Decade of Movies/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  A Decade of Movies
+//
+//  Created by Nesreen Mamdouh on 5/19/19.
+//  Copyright © 2019 swvl. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieRating: UILabel!
+    @IBOutlet weak var movieName: UILabel!
+   
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
