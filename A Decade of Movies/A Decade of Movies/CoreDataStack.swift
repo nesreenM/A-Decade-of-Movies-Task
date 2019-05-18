db9234e56b6694339d714ebe469bc17abb027a7e
Nesreen Mamdouh <>
2019-05-19 01:29:19 +0200

    Added coredata singleton

---
+++ A Decade of Movies/A Decade of Movies/CoreDataStack.swift
@@ -0,0 +1,41 @@
+//
+//  CoreDataStack.swift
+//  A Decade of Movies
+//
+//  Created by Nesreen Mamdouh on 5/19/19.
+//  Copyright © 2019 swvl. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack: NSObject {
+
+    static let sharedInstance = CoreDataStack()
+    private override init() {}
+    
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TestJsonFile")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
