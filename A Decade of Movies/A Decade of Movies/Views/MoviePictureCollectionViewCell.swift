35d30ed362ded790bd53875572621ac3c9617715
Nesreen Mamdouh <>
2019-05-19 12:31:21 +0200

    Fix creating image url bug

--- A Decade of Movies/A Decade of Movies/Views/MoviePictureCollectionViewCell.swift
+++ A Decade of Movies/A Decade of Movies/Views/MoviePictureCollectionViewCell.swift
@@ -25,12 +25,12 @@ class MoviePictureCollectionViewCell: UICollectionViewCell {
     }
     
     func bindData() {
-        let farm = moviePictureCellViewModel.photo.farm ?? 0
-        let server = moviePictureCellViewModel.photo.server ?? ""
-        let id = moviePictureCellViewModel.photo.id ?? ""
-        let secret = moviePictureCellViewModel.photo.secret ?? ""
-        let url = "https://farm​\(farm)​.static.flickr.com/​\(server)​/​\(id)_​\(secret)​.jpg"
-//        let url = "https://farm66.static.flickr.com/65535/32885403967_be14b95a9c.jpg"
-        moviePicture.setImage(url: url)
+        if let farm = moviePictureCellViewModel.photo.farm,
+            let server = moviePictureCellViewModel.photo.server,
+            let id = moviePictureCellViewModel.photo.id,
+            let secret = moviePictureCellViewModel.photo.secret {
+            let url = "https://farm\(farm).static.flickr.com/\(server)/\(id)_\(secret).jpg"
+            moviePicture.setImage(url: url)
+        }
     }
 }
